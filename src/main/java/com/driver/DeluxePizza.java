8eb3703e4d8190be2dce0a0e8d942a892afd6f6d
AbdulMajeedShaik <shaika854@gmail.com>
2023-10-17 19:15:55 +0530

    changes done

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,11 @@
 package com.driver;
-
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
+    public DeluxePizza(boolean isVeg) {
         super(isVeg);
-        // your code goes here
+
+        // Add extra cheese and toppings by default
+        this.addExtraCheese();
+        this.addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,32 +3,53 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    private boolean isVeg;
     private String bill;
 
-    public Pizza(Boolean isVeg){
+    // Constants for pricing
+    private static final int VEG_BASE_PRICE = 300;
+    private static final int NON_VEG_BASE_PRICE = 400;
+    private static final int EXTRA_CHEESE_PRICE = 80;
+    private static final int VEG_EXTRA_TOPPINGS_PRICE = 70;
+    private static final int NON_VEG_EXTRA_TOPPINGS_PRICE = 120;
+    private static final int PAPER_BAG_PRICE = 20;
+
+    public Pizza(boolean isVeg) {
         this.isVeg = isVeg;
-        // your code goes here
+        this.price = isVeg ? VEG_BASE_PRICE : NON_VEG_BASE_PRICE;
+        this.bill = "Base Price Of The Pizza: " + this.price;
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese() {
+        if (!bill.contains("Extra Cheese Added")) {
+            this.price += EXTRA_CHEESE_PRICE;
+            this.bill += "\nExtra Cheese Added: " + EXTRA_CHEESE_PRICE;
+        }
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings() {
+        if (isVeg && !bill.contains("Extra Toppings Added")) {
+            this.price += VEG_EXTRA_TOPPINGS_PRICE;
+            this.bill += "\nExtra Toppings Added: " + VEG_EXTRA_TOPPINGS_PRICE;
+        } else if (!isVeg && !bill.contains("Extra Toppings Added")) {
+            this.price += NON_VEG_EXTRA_TOPPINGS_PRICE;
+            this.bill += "\nExtra Toppings Added: " + NON_VEG_EXTRA_TOPPINGS_PRICE;
+        }
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway() {
+        if (!bill.contains("Paperbag Added")) {
+            this.price += PAPER_BAG_PRICE;
+            this.bill += "\nPaperbag Added: " + PAPER_BAG_PRICE;
+        }
     }
 
-    public String getBill(){
-        // your code goes here
+    public String getBill() {
+        this.bill += "\nTotal Price: " + this.price;
         return this.bill;
     }
 }
